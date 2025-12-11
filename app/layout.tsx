401afcf27e76259ec0114dc9246d6af208f01f71
Ransika Silva <ransikasilva2003.03.22@gmail.com>
2025-12-11 09:42:57 +0530

    Import Tldraw CSS via intermediate CSS file to force inclusion in build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 
 import { Inter } from 'next/font/google';
 import './globals.css';
-import 'tldraw/tldraw.css';
+import './tldraw-styles.css';
 import MainNavigation from '@/components/layout/MainNavigation';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
